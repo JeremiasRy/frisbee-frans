a7911dcf3341a2309e885cfdb12ea354120fa533
Jeremias Ryttäri <jeremias.ryttari@innokasmedical.fi>
2023-09-28 13:31:35 +0300

    feat: design

--- frontend/src/pages/ScoreInput.tsx
+++ frontend/src/pages/ScoreInput.tsx
@@ -1,11 +1,14 @@
-import { Outlet, useNavigate, useParams } from "react-router-dom";
+import { Outlet, useLocation, useNavigate, useParams } from "react-router-dom";
 import { useAppDispatch, useAppSelector } from "../redux/hooks";
 import { SetStateAction, useEffect, useState } from "react";
 import { getRound } from "../redux/reducer/roundReducer";
 import RoundCard from "../components/RoundCard";
-import { Box, Button, Typography } from "@mui/material";
+import { Box, Button, IconButton, Typography } from "@mui/material";
 import { ResultInput } from "../components/ResultInput";
 import { Hole } from "../types/models";
+import RoundNthHoleIndicator from "../components/RoundNthHoleIndicator";
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 
 export default function ScoreInput() {
     const { id, nthHole } = useParams();
@@ -14,7 +17,7 @@ export default function ScoreInput() {
     const navigate = useNavigate();
     const [throws, setThrows] = useState(0);
     const [penalties, setPenalties] = useState(0);
-    
+    type Direction = "Next" | "Previous";
 
     useEffect(() => {
         const controller = new AbortController();
@@ -41,6 +44,9 @@ export default function ScoreInput() {
     ? course.holes.find(hole => hole.nthHole === parseInt(nthHole)) as Hole 
     : undefined
 
+    function HandleNavigationButtonChange(direction:Direction) {
+    }
+
     return (
         <Box sx={{
             display: "flex",
@@ -53,19 +59,22 @@ export default function ScoreInput() {
             {nthHole === undefined && <Button onClick={() => navigate("1")}>Go to first hole?</Button>}
             {hole &&
             <Box sx={{
-                width: "100%"
+                width: "100%",
+                display: "flex",
+                flexDirection: "column",
+                rowGap: "2em"
             }}>
                 <Outlet />
+                <RoundNthHoleIndicator count={course.holes.length} />
                 <Box sx={{
                     display: "flex",
                     flexDirection: "row",
                     justifyContent: "space-around"
                 }}>
-                <Button>Previous</Button>
-                <ResultInput par={hole.par} throws={throws} setThrows={setThrows} penalties={penalties} setPenalties={setPenalties} />
-                <Button>Next</Button>
+                    <IconButton size="large"><ArrowBackIosIcon/></IconButton>
+                    <ResultInput par={hole.par} throws={throws} setThrows={setThrows} penalties={penalties} setPenalties={setPenalties} />
+                    <IconButton size="large"><ArrowForwardIosIcon/></IconButton>
                 </Box>
-                
             </Box>
             }
             <RoundCard round={roundReducer.entities[0]} localResults={copyOfRound} />
