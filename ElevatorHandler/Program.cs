638f1ce2150556f60e2f709f76a3f7a8a8e2e456
Jeremias Ryttäri <jeremias.ryttari@innokasmedical.fi>
2023-10-06 14:32:23 +0300

    feat: initialize project.. experiments

---
+++ ElevatorHandler/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ElevatorHandler;
+
+ScreenBuffer buffer = new ();
+Graphic human = new("../../../Assets/Human.txt");
+Graphic elevator = new("../../../Assets/Elevator.txt");
+Graphic backDrop = new();
+
+buffer.WriteGraphicToBuffer(backDrop.Picture, 0, 0);
+buffer.WriteGraphicToBuffer(human.Picture, 12, 3);
+buffer.WriteGraphicToBuffer(elevator.Picture, 15, 3);
+buffer.DrawBuffer();
