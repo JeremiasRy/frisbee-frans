4f7dd239a7c927d424f189f9328ede6188ca5966
Jeremias Ryttäri <jeremias.ryttari@innokasmedical.fi>
2023-10-16 11:21:03 +0300

    feat: sw writers for info scraper

--- initData/InformationScraper/Program.cs
+++ initData/InformationScraper/Program.cs
@@ -5,9 +5,12 @@ using System.Text.Json;
 using System.Text.Unicode;
 using static InformationScraper.FileParsingMethods;
 
+
 Console.WriteLine("Fetching links...");
 var linkBank = LinkBank.GetInstance("https://frisbeegolfradat.fi/radat/");
 await linkBank.InitializeLinks();
+var citiesParser = new LinkParser(await RequestHandler.GetHttpResponse("https://frisbeegolfradat.fi/radat/"));
+List<string> cities = citiesParser.CitiesFromIndex();
 List<Worker> workers = new ();
 
 for (var i = 0; i < 5; i++)
@@ -17,4 +20,15 @@ for (var i = 0; i < 5; i++)
 
 var tasks = workers.AsParallel().Select(worker => worker.DoWork()).ToArray();
 var results = await Task.WhenAll(tasks);
-Console.WriteLine(results.Length);
+
+using (var sw = new StreamWriter("../../../cities.json"))
+{
+    var json = JsonSerializer.Serialize(cities);
+    sw.Write(json);
+}
+using (var sw = new StreamWriter("../../../courses.json"))
+{
+    var json = JsonSerializer.Serialize(results);
+    sw.Write(json);
+};
+
